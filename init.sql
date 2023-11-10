6ef5c4df499953b4fb383f2037c02d5dcb925835
erenculhaci <erenculhaci@hotmail.com>
2023-11-10 17:49:09 +0300

    Pages for races and drivers are created.

--- init.sql
+++ init.sql
@@ -1,5 +1,54 @@
 USE mysql;
 
+CREATE TABLE IF NOT EXISTS races (
+    raceId INT,
+    year INT,
+    round INT,
+    circuitId INT,
+    name VARCHAR(50),
+    date DATE,
+    time TIME,
+    url VARCHAR(100),
+    fp1_date DATE,
+    fp1_time TIME,
+    fp2_date DATE,
+    fp2_time TIME,
+    fp3_date DATE,
+    fp3_time TIME,
+    quali_date DATE,
+    quali_time TIME,   
+    sprint_date DATE,
+    sprint_time TIME
+);
+
+-- Load data from CSV file into the table
+LOAD DATA LOCAL INFILE '/data/races.csv'
+INTO TABLE races
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+CREATE TABLE IF NOT EXISTS drivers (
+    driverId INT,
+    drivrRef VARCHAR(20),
+    number INT,
+    code CHAR(3),
+    forename VARCHAR(50),
+    surname VARCHAR(50),
+    dob DATE,
+    nationality VARCHAR(20),
+    url VARCHAR(100)
+);
+
+-- Load data from CSV file into the table
+LOAD DATA LOCAL INFILE '/data/drivers.csv'
+INTO TABLE drivers
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 CREATE TABLE IF NOT EXISTS pit_stops (
     raceId INT,
     driverId INT,
